fd81f5cc3cc3dd8e9adc68a38ad6e301187ee187
Dion Chaika <49035785+dionchaika@users.noreply.github.com>
2019-04-03 18:28:55 +0300

    Add Query class

---
+++ src/Query/Query.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * The PHP Database Library.
+ *
+ * @package dionchaika/db
+ * @version 1.0.0
+ * @license MIT
+ * @author Dion Chaika <dionchaika@gmail.com>
+ */
+
+namespace Dionchaika\Db\Query;
+
+class Query
+{
+    
+}
