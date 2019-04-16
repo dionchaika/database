a3934455fa00802409ab22a352e77aebc3c8c89f
Dion Chaika <49035785+dionchaika@users.noreply.github.com>
2019-04-16 13:54:05 +0300

    Update MySqlCompiler.php

--- src/Query/Compilers/MySqlCompiler.php
+++ src/Query/Compilers/MySqlCompiler.php
@@ -34,36 +34,6 @@ class MySqlCompiler implements CompilerInterface
     ];
 
     /**
-     * Quote an SQL name.
-     *
-     * Grammar:
-     *      *|`sql_name`.
-     *
-     * @param string $name
-     * @return string
-     */
-    public function quoteName(string $name): string
-    {
-        return ('*' === $name)
-            ? $name
-            : '`'.str_replace('`', '\\`', $name).'`';
-    }
-
-    /**
-     * Quote an SQL string.
-     *
-     * Grammar:
-     *      'sql_string'.
-     *
-     * @param string $string
-     * @return string
-     */
-    public function quoteString(string $string): string
-    {
-        return '\''.str_replace('\'', '\\\'', $string).'\'';
-    }
-
-    /**
      * Compile an SQL name.
      *
      * Grammar:
@@ -81,40 +51,6 @@ class MySqlCompiler implements CompilerInterface
         }
 
         return $this->compileNameComponents($name);
-    }
-
-    /**
-     * Compile an aliased SQL name.
-     *
-     * Grammar:
-     *      sql_name_components AS `sql_name`.
-     *
-     * @param string $aliasedName
-     * @return string
-     */
-    public function compileAliasedName(string $aliasedName): string
-    {
-        $aliasedNameParts = preg_split('/\s+as\s+/i', $aliasedName);
-
-        $name = $aliasedNameParts[0];
-        $alias = $aliasedNameParts[1];
-
-        return "{$this->compileNameComponents($name)} AS {$this->quoteName($alias)}";
-    }
-
-    /**
-     * Compile an SQL name components.
-     *
-     * Grammar:
-     *      [`sql_name`.]`sql_name` or
-     *      [[`sql_name`.]`sql_name`.]`sql_name`.
-     *
-     * @param string $nameComponents
-     * @return string
-     */
-    public function compileNameComponents(string $nameComponents): string
-    {
-        return implode('.', array_map(['static', 'quoteName'], preg_split('/\s*\.\s*/', $nameComponents, 3)));
     }
 
     /**
@@ -151,5 +87,69 @@ class MySqlCompiler implements CompilerInterface
         }
 
         return $this->quoteString($value);
+    }
+
+    /**
+     * Quote an SQL name.
+     *
+     * Grammar:
+     *      *|`sql_name`.
+     *
+     * @param string $name
+     * @return string
+     */
+    protected function quoteName(string $name): string
+    {
+        return ('*' === $name)
+            ? $name
+            : '`'.str_replace('`', '\\`', $name).'`';
+    }
+
+    /**
+     * Quote an SQL string.
+     *
+     * Grammar:
+     *      'sql_string'.
+     *
+     * @param string $string
+     * @return string
+     */
+    protected function quoteString(string $string): string
+    {
+        return '\''.str_replace('\'', '\\\'', $string).'\'';
+    }
+
+    /**
+     * Compile an aliased SQL name.
+     *
+     * Grammar:
+     *      sql_name_components AS `sql_name`.
+     *
+     * @param string $aliasedName
+     * @return string
+     */
+    protected function compileAliasedName(string $aliasedName): string
+    {
+        $aliasedNameParts = preg_split('/\s+as\s+/i', $aliasedName);
+
+        $name = $aliasedNameParts[0];
+        $alias = $aliasedNameParts[1];
+
+        return "{$this->compileNameComponents($name)} AS {$this->quoteName($alias)}";
+    }
+
+    /**
+     * Compile an SQL name components.
+     *
+     * Grammar:
+     *      [`sql_name`.]`sql_name` or
+     *      [[`sql_name`.]`sql_name`.]`sql_name`.
+     *
+     * @param string $nameComponents
+     * @return string
+     */
+    protected function compileNameComponents(string $nameComponents): string
+    {
+        return implode('.', array_map(['static', 'quoteName'], preg_split('/\s*\.\s*/', $nameComponents, 3)));
     }
 }
