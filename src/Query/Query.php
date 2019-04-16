3fb8f4339ac838767ca0ad7314123ca007d40505
Dion Chaika <49035785+dionchaika@users.noreply.github.com>
2019-04-16 11:34:33 +0300

    Update Query.php

--- src/Query/Query.php
+++ src/Query/Query.php
@@ -13,5 +13,18 @@ namespace Dionchaika\Database\Query;
 
 class Query
 {
-    
+    /**
+     * The query compiler.
+     *
+     * @var \Dionchaika\Database\CompilerInterface
+     */
+    protected $compiler;
+
+    /**
+     * @param \Dionchaika\Database\CompilerInterface $compiler
+     */
+    public function __construct(CompilerInterface $compiler)
+    {
+        $this->compiler = $compiler;
+    }
 }
