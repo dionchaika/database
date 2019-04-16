1917d99ce1a04b2d9dc461a7b6d90e1d4e3eaa3e
Dion Chaika <49035785+dionchaika@users.noreply.github.com>
2019-04-16 11:41:05 +0300

    Update Query.php

--- src/Query/Query.php
+++ src/Query/Query.php
@@ -14,11 +14,55 @@ namespace Dionchaika\Database\Query;
 class Query
 {
     /**
+     * The query SELECT statement type.
+     */
+    const TYPE_SELECT = 0;
+
+    /**
+     * The query INSERT statement type.
+     */
+    const TYPE_INSERT = 1;
+
+    /**
+     * The query UPDATE statement type.
+     */
+    const TYPE_UPDATE = 2;
+
+    /**
+     * The query DELETE statement type.
+     */
+    const TYPE_DELETE = 3;
+
+    /**
      * The query compiler.
      *
      * @var \Dionchaika\Database\CompilerInterface
      */
     protected $compiler;
+
+    /**
+     * The query statement type.
+     *
+     * @var int
+     */
+    protected $type;
+
+    /**
+     * The array of query parts.
+     *
+     * @var array
+     */
+    protected $parts = [
+        'select' => [],
+        'from' => null,
+        'where' => [],
+        'orderBy' => [],
+        'limit' => null,
+        'into' => null,
+        'values' => [],
+        'update' => null,
+        'set' => []
+    ];
 
     /**
      * @param \Dionchaika\Database\CompilerInterface $compiler
