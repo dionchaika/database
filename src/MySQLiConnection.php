8f32ed403c2ffb5ed517929ef872fb78f53e5bee
Dion Chaika <49035785+dionchaika@users.noreply.github.com>
2019-04-19 19:50:08 +0300

    Create MySQLiConnection.php

---
+++ src/MySQLiConnection.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * The PHP Database Library.
+ *
+ * @package dionchaika/database
+ * @version 1.0.0
+ * @license MIT
+ * @author Dion Chaika <dionchaika@gmail.com>
+ */
+
+namespace Dionchaika\Database;
+
+use mysqli;
+use mysqli_stmt;
+use mysqli_result;
+use Dionchaika\Database\QueryException;
+use Dionchaika\Database\FetchException;
+
+class MySQLiConnection implements ConnectionInterface
+{
+    //
+}
