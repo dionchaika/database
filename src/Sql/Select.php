1b8e8597147eda6824a34555ee89b0b2850fe884
Dion Chaika <49035785+dionchaika@users.noreply.github.com>
2019-05-27 18:05:28 +0300

    Update Select.php

--- src/Sql/Select.php
+++ src/Sql/Select.php
@@ -11,6 +11,13 @@ namespace Lazy\Db\Sql;
 class Select
 {
     /**
+     * The SELECT statement
+     * ORDER BY clause orders.
+     */
+    const ORDER_ASC = 'ASC';
+    const ORDER_DESC = 'DESC';
+
+    /**
      * The array
      * of SELECT statement parts.
      *
@@ -37,7 +44,8 @@ class Select
     }
 
     /**
-     * Make the SELECT statement distinct.
+     * Make the SELECT
+     * statement distinct.
      *
      * @return self
      */
@@ -48,7 +56,8 @@ class Select
     }
 
     /**
-     * Set the SELECT statement table.
+     * Set the SELECT
+     * statement table.
      *
      * @param mixed $tableName
      * @return self
@@ -57,5 +66,47 @@ class Select
     {
         $this->parts['from'] = $this->compileName($tableName);
         return $this;
+    }
+
+    /**
+     * Add the SELECT
+     * statement ORDER BY clause.
+     *
+     * @param mixed[]|mixed $columnNames
+     * @param string        $order
+     * @return self
+     */
+    public function orderBy($columnNames, string $order = self::ORDER_ASC): self
+    {
+        $columnNames = is_array($columnNames) ? $columnNames : [$columnNames];
+        $this->parts['orderBy'][] = implode(', ', array_map(['static', 'compileName'], $columnNames)).' '.$order;
+
+        return $this;
+    }
+
+    /**
+     * Add the SELECT
+     * statement ORDER BY ASC clause.
+     *
+     * @param mixed $columnNames
+     * @return self
+     */
+    public function orderByAsc($columnNames): self
+    {
+        $columnNames = is_array($columnNames) ? $columnNames : func_get_args();
+        return $this->orderBy($columnNames, self::ORDER_ASC);
+    }
+
+    /**
+     * Add the SELECT
+     * statement ORDER BY DESC clause.
+     *
+     * @param mixed $columnNames
+     * @return self
+     */
+    public function orderByDesc($columnNames): self
+    {
+        $columnNames = is_array($columnNames) ? $columnNames : func_get_args();
+        return $this->orderBy($columnNames, self::ORDER_DESC);
     }
 }
